57a9add26dbe0c3a15a85676550b3f305ec1f937
StarExpedition <211250147@smail.nju.edu.cn>
2023-07-20 16:31:07 +0800

    Init hertz frame

---
+++ idl/teacher.thrift
@@ -0,0 +1,29 @@
+namespace go teacher
+
+//--------------------request & response--------------
+struct College {
+    1: required string name(go.tag = 'json:"name"'),
+    2: string address(go.tag = 'json:"address"'),
+}
+
+struct Teacher {
+    1: required i32 id(api.body='id'),
+    2: required string name(api.body='name'),
+    3: required College college(api.body='college'),
+}
+
+struct RegisterResp {
+    1: bool success(api.body='success'),
+    2: string message(api.body='message'),
+}
+
+struct QueryReq {
+    1: required i32 id(api.query='id')
+}
+
+
+//----------------------service-------------------
+service TeacherService {
+    RegisterResp Register(1: Teacher theTeacher)(api.post = '/add-teacher-info')
+    Teacher Query(1: QueryReq req)(api.get = '/query-teacher-info')
+}
